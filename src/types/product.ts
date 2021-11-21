667019758e89a2ab1260dde319d132cc72f2e84a
leminhthanh142 <thanhle12345789@gmail.com>
2021-11-21 23:30:27 +0700

    add product type

---
+++ src/types/product.ts
@@ -0,0 +1,10 @@
+export type ProductData = {
+  id: number,
+  title: string,
+  imgLink: string,
+  description: string,
+  price: number,
+  productSold: number,
+  discount: number,
+  discountImg: string
+}
