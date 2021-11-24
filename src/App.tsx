dbc4861ec65b420cf5265518d55d14585b30048a
leminhthanh142 <thanhle12345789@gmail.com>
2021-11-24 10:02:01 +0700

    add husky and lint-staged

--- src/App.tsx
+++ src/App.tsx
@@ -15,7 +15,8 @@ export const App = () => (
       <Route exact path="/signup" component={SignUpPage} />
       <Route exact path="/signin" component={SignInPage} />
       <Route exact path="/mall" component={ShopeeMall} />
-      <Route path="/product/:id" component={ProductDetails} />
+      <Route path="/product/:id"
+             component={ProductDetails} />
       <Redirect from="*" to="/" />
     </Switch>
     <GlobalStyle />
