7df0d6ba60a1f906306a6b680696d6b2d5d11ef7
doremongu <hiiamgosu4@gmail.com>
2021-11-24 14:46:41 +0700

    support page

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import { Switch, Route, BrowserRouter, Redirect } from "react-router-dom";
 import { HomePage } from "./components/pages/HomePage";
 import { SignInPage } from "./components/pages/SignIn";
 import { SignUpPage } from "./components/pages/SignUp";
+import { SupportPage } from "./components/pages/Support";
 import { GlobalStyle } from "./GlobalStyle";
 import { ShopeeMall } from "./components/pages/ShopeeMall";
 import { ProductDetails } from "./components/pages/ProductDetails";
@@ -14,11 +15,9 @@ export const App = () => (
       <Route exact path="/" component={HomePage} />
       <Route exact path="/signup" component={SignUpPage} />
       <Route exact path="/signin" component={SignInPage} />
+      <Route exact path="/support" component={SupportPage} />
       <Route exact path="/mall" component={ShopeeMall} />
-      <Route
-        path="/product/:id"
-        component={ProductDetails}
-      />
+      <Route path="/product/:id" component={ProductDetails} />
       <Redirect from="*" to="/" />
     </Switch>
     <GlobalStyle />
